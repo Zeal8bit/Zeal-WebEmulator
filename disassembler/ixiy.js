8cf08bd13f4a721ee51a50494987a8f3abfe6257
Zeal8bit <contact@zeal8bit.com>
2023-07-14 23:56:40 +0800

    disassembler: fix IX/IY instructions disassembly

--- disassembler/ixiy.js
+++ disassembler/ixiy.js
@@ -1,6 +1,6 @@
 /**
- * SPDX-FileCopyrightText: 2022 Zeal 8-bit Computer <contact@zeal8bit.com>
- * 
+ * SPDX-FileCopyrightText: 2023 Zeal 8-bit Computer <contact@zeal8bit.com>
+ *
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -8,287 +8,218 @@ const intructions_iy_op = 0xFD;
 const intructions_ix_op = 0xDD;
 
 const hex = str => str.toString(16)
-const reg_idx = ["B", "C", "D", "E", "H", "L", "A"];
 
-const intructions = [
-    {
-        name: "ADC A,(r+o)",
-        check: (snd_op) => snd_op == 0x8E,
+/* The following object doesn't include CB nor LD (r+n), r which are special cases */
+const dd_fd_intructions = {
+    0x8E: {
         text: (thrd_op) => `ADC A,(r+\$${hex(thrd_op)})`,
         size: 3
     },
-    {
-        name: "ADD A,(r+o)",
-        check: (snd_op) => snd_op == 0x86,
+    0x86: {
         text: (thrd_op) => `ADD A,(r+\$${hex(thrd_op)})`,
         size: 3
     },
-    {
-        name: "ADD r,BC",
-        check: (snd_op) => snd_op == 0x9,
+    0x9: {
         text: () => `ADD r,BC`,
         size: 2
     },
-    {
-        name: "ADD r,DE",
-        check: (snd_op) => snd_op == 0x19,
+    0x19: {
         text: () => `ADD r,DE`,
         size: 2
     },
-    {
-        name: "ADD r,r",
-        check: (snd_op) => snd_op == 0x29,
+    0x29: {
         text: () => `ADD r,r`,
         size: 2
     },
-    {
-        name: "ADD r,SP",
-        check: (snd_op) => snd_op == 0x39,
+    0x39: {
         text: () => `ADD r,SP`,
         size: 2
     },
-    {
-        name: "AND (r+o)",
-        check: (snd_op) => snd_op == 0xA6,
+    0xA6: {
         text: (thrd_op) => `AND (r+\$${hex(thrd_op)})`,
         size: 3
     },
-    {
-        name: "BIT b,(r+o)",
-        check: (snd_op, thrd_op, forth_op) =>
-            snd_op == 0xCB && (forth_op & 0xc7) == 0x46,
-        text: (thrd_op, for_op) => `BIT ${(0x7e / 8) & 0x7},(r+\$${hex(thrd_op)})`,
-        size: 4
-    },
-    {
-        name: "CP (r+o)",
-        check: (snd_op) => snd_op == 0xBE,
+    0xBE: {
         text: (thrd_op) => `CP (r+\$${hex(thrd_op)})`,
         size: 3
     },
-    {
-        name: "DEC (r+o)",
-        check: (snd_op) => snd_op == 0x35,
+    0x35: {
         text: (thrd_op) => `DEC (r+\$${hex(thrd_op)})`,
         size: 3
     },
-    {
-        name: "DEC r",
-        check: (snd_op) => snd_op == 0x2B,
+    0x2B: {
         text: () => `DEC r`,
         size: 2
     },
-    {
-        name: "EX (SP),r",
-        check: (snd_op) => snd_op == 0xE3,
+    0xE3: {
         text: () => `EX (SP),r`,
         size: 2
     },
-    {
-        name: "INC (r+o)",
-        check: (snd_op) => snd_op == 0x34,
+    0x34: {
         text: (thrd_op) => `INC (r+\$${hex(thrd_op)})`,
         size: 3
     },
-    {
-        name: "INC r",
-        check: (snd_op) => snd_op == 0x23,
+    0x23: {
         text: () => `INC r`,
         size: 2
     },
-    {
-        name: "JP (r)",
-        check: (snd_op) => snd_op == 0xE9,
+    0xE9: {
         text: () => `JP (r)`,
         size: 2
     },
-    {
-        name: "LD (r+o),n",
-        check: (snd_op) => snd_op == 0x36,
+    0x36: {
         text: (thrd_op, forth_op) => `LD (r+\$${hex(thrd_op)}),\$${hex(forth_op)}`,
         size: 4
     },
-    {
-        name: "LD (r+o),r",
-        check: (snd_op) => (snd_op & 0xf8) == 0x70,
-        text: (thrd_op) => `LD (r+\$${hex(thrd_op)}),{0}`,
-        post: (str, snd) => str.replace("{0}", reg_idx[snd & 0x7]),
-        size: 3
-    },
-    {
-        name: "LD (nn),r",
-        check: (snd_op) => snd_op == 0x22,
-        text: (thrd_op, forth_op) => `LD (\$${hex(forth << 8 | thrd_op)}),r`,
+    0x22: {
+        text: (thrd_op, forth_op) => `LD (\$${hex(forth_op << 8 | thrd_op)}),r`,
         size: 4
     },
-    {
-        name: "LD A,(r+o)",
-        check: (snd_op) => snd_op == 0x7E,
+    0x7E: {
         text: (thrd_op) => `LD A,(r+\$${hex(thrd_op)})`,
         size: 3
     },
-    {
-        name: "LD B,(r+o)",
-        check: (snd_op) => snd_op == 0x46,
+    0x46: {
         text: (thrd_op) => `LD B,(r+\$${hex(thrd_op)})`,
         size: 3
     },
-    {
-        name: "LD C,(r+o)",
-        check: (snd_op) => snd_op == 0x4E,
+    0x4E: {
         text: (thrd_op) => `LD C,(r+\$${hex(thrd_op)})`,
         size: 3
     },
-    {
-        name: "LD D,(r+o)",
-        check: (snd_op) => snd_op == 0x56,
+    0x56: {
         text: (thrd_op) => `LD D,(r+\$${hex(thrd_op)})`,
         size: 3
     },
-    {
-        name: "LD E,(r+o)",
-        check: (snd_op) => snd_op == 0x5E,
+    0x5E: {
         text: (thrd_op) => `LD E,(r+\$${hex(thrd_op)})`,
         size: 3
     },
-    {
-        name: "LD H,(r+o)",
-        check: (snd_op) => snd_op == 0x66,
+    0x66: {
         text: (thrd_op) => `Ld H,(r+\$${hex(thrd_op)})`,
         size: 3
     },
-    {
-        name: "LD r,(nn)",
-        check: (snd_op) => snd_op == 0x2A,
+    0x2A: {
         text: (thrd_op, forth_op) => `LD r,(\$${hex(forth_op << 8 | thrd_op)})`,
         size: 4
     },
-    {
-        name: "LD r,nn",
-        check: (snd_op) => snd_op == 0x21,
+    0x21: {
         text: (thrd_op, forth_op) => `LD r,\$${hex(forth_op << 8 | thrd_op)}`,
         size: 4
     },
-    {
-        name: "LD L,(r+o)",
-        check: (snd_op) => snd_op == 0x6E,
+    0x6E: {
         text: (thrd_op) => `LD L,(r+\$${hex(thrd_op)})`,
         size: 3
     },
-    {
-        name: "LD SP,r",
-        check: (snd_op) => snd_op == 0xF9,
+    0xF9: {
         text: () => `LD SP,r`,
         size: 2
     },
-    {
-        name: "OR (r+o)",
-        check: (snd_op) => snd_op == 0xB6,
+    0xB6: {
         text: (thrd_op) => `OR (r+\$${hex(thrd_op)})`,
         size: 3
     },
-    {
-        name: "POP r",
-        check: (snd_op) => snd_op == 0xE1,
+    0xE1: {
         text: () => `POP r`,
         size: 2
     },
-    {
-        name: "PUSH r",
-        check: (snd_op) => snd_op == 0xE5,
+    0xE5: {
         text: () => `PUSH r`,
         size: 2
     },
-    {
-        name: "RES b,(r+o)",
-        opcode: "CB o 86+8*b",
-        size: 4
-    },
-    {
-        name: "RL (r+o)",
-        check: (snd_op, thr_op, for_op) => snd_op == 0xCB && for_op == 0x16,
-        text: (thrd_op) => `RL (r+\$${hex(thrd_op)})`,
-        size: 4
-    },
-    {
-        name: "RLC (r+o)",
-        check: (snd_op, thr_op, for_op) => snd_op == 0xCB && for_op == 0x06,
-        text: (thrd_op) => `RLC (r+\$${hex(thrd_op)})`,
-        size: 4
-    },
-    {
-        name: "RR (r+o)",
-        check: (snd_op, thr_op, for_op) => snd_op == 0xCB && for_op == 0x1E,
-        text: (thrd_op) => `RR (r+\$${hex(thrd_op)})`,
-        size: 4
-    },
-    {
-        name: "RRC (r+o)",
-        check: (snd_op, thr_op, for_op) => snd_op == 0xCB && for_op == 0x0E,
-        text: (thrd_op) => `RRC (r+\$${hex(thrd_op)})`,
-        size: 4
-    },
-    {
-        name: "SBC A,(r+o)",
-        check: (snd_op) => snd_op == 0x9E,
-        text: (thrd_op) => `SBC A,(r+\$${hex(thrd_op)})`,
-        size: 3
-    },
-    {
-        name: "SET b,(r+o)",
-        check: (snd_op, thr_op, for_op) =>
-            snd_op == 0xCB && (forth_op & 0xc7) == 0xc6,
-        text: (thrd_op, fort_op) =>
-            `SET ${(fort_op >> 3) & 0x7},(r+\$${hex(thrd_op)})`,
-        size: 4
-    },
-    {
-        name: "SLA (r+o)",
-        check: (snd_op, thr_op, for_op) => snd_op == 0xCB && for_op == 0x26,
-        text: (thrd_op) => `SLA (r+\$${hex(thrd_op)})`,
-        size: 4
-    },
-    {
-        name: "SRA (r+o)",
-        check: (snd_op, thr_op, for_op) => snd_op == 0xCB && for_op == 0x2E,
-        text: (thrd_op) => `SRA (r+\$${hex(thrd_op)})`,
-        size: 4
-    },
-    {
-        name: "SRL (r+o)",
-        check: (snd_op, thr_op, for_op) => snd_op == 0xCB && for_op == 0x3E,
-        text: (thrd_op) => `SRL (r+\$${hex(thrd_op)})`,
-        size: 4
-    },
-    {
-        name: "SUB (r+o)",
-        check: (snd_op) => snd_op == 0x96,
+    0x96: {
         text: (thrd_op) => `SUB (r+\$${hex(thrd_op)})`,
         size: 3
     },
-    {
-        name: "XOR (r+o)",
-        check: (snd_op) => snd_op == 0xAE,
+    0xAE: {
         text: (thrd_op) => `XOR (r+\$${hex(thrd_op)})`,
         size: 3
-    }
-];
+    },
+    0x9E: {
+        text: (thrd_op) => `SBC A,(r+\$${hex(thrd_op)})`,
+        size: 3
+    },
+};
 
-function dis_ix_iy(binary, index) {
-    const fst = binary[index + 0];
-    const snd = binary[index + 1];
-    const trd = binary[index + 2];
-    const fot = binary[index + 3];
 
-    if (opcode == intructions_iy_op || opcode == intructions_ix_op) {
-        const reg = (opcode == intructions_iy_op) ? "IY" : "IX";
-        for (var i = 0; i < intructions.length; i++) {
-            const entry = instructions[0];
-            if (entry.check(snd, trd, fot)) {
-                return { text: entry.text(trd, fot), size: entry.size };
+function opcode_DD_FD_CB_x(third, fourth)
+{
+    switch (fourth){
+        case 0x06: return `RLC (r+\$${hex(third)})`;
+        case 0x0e: return `RRC (r+\$${hex(third)})`;
+        case 0x16: return `RL  (r+\$${hex(third)})`;
+        case 0x1e: return `RR  (r+\$${hex(third)})`;
+        case 0x26: return `SLA (r+\$${hex(third)})`;
+        case 0x2e: return `SRA (r+\$${hex(third)})`;
+        case 0x3e: return `SRL (r+\$${hex(third)})`;
+        default:
+            if ((fourth & 0xc7) == 0x46) {
+                const bit = (fourth >> 8) & 0x7;
+                return `BIT ${bit},(r+\$${hex(third)})`;
+            } else if ((fourth & 0xc7) == 0xc) {
+                return `SET ${(fourth >> 3) & 0x7},(r+\$${hex(third)})`;
             }
-        }
+            return 'ILL';
+    };
+}
+
+
+/**
+ * @brief Process the given opcodes, regardless of IX or IY since the instructions are the same.
+ *
+ * @returns Object of the form:
+        { text: <disassembled_instruction>, size: n }
+ */
+function opcode_DD_FD_x(second, third, fourth) {
+    const entry = dd_fd_intructions[second];
+
+    /* Check if the entry exists for the given 'second' opcode, if not, check for the special cases */
+    if (entry) {
+
+        /* Execute the text() function part of the entry */
+        return { text: entry.text(third, fourth), size: entry.size };
+
+    } else if (second == 0xCB) {
+
+        const text = opcode_DD_FD_CB_x(third, fourth);
+        return { text, size: 4 };
+
+    } else if ((second & 0xf8) == 0x70) {
+
+        /* Special case because the opcode contains the parameter */
+        const reg_idx = ["B", "C", "D", "E", "H", "L", "A"];
+
+        const text = `LD (r+\$${hex(third)}), ${reg_idx[second & 0x7]}`;
+        return { text, size: 3 };
+
     }
 
-    return null;
+    /* Unknown instruction */
+    return { text: 'ILL', size: '2' };
+}
+
+
+/**
+ * @brief Get the string equivalent of the given opcode (which is preceded by 0xDD)
+ *
+ * @returns Object of the form:
+ *          { text: <disassembled_instruction>, size: n }
+ */
+function opcode_DD_x(second, third, fourth) {
+    const { text, size } = opcode_DD_FD_x(second, third, fourth);
+    /* Replace 'r' with IX */
+    return { text: text.replace('r', 'IX'), size };
+}
+
+
+/**
+ * @brief Get the string equivalent of the given opcode (which is preceded by 0xDD)
+ *
+ * @returns Object of the form:
+ *          { text: <disassembled_instruction>, size: n }
+ */
+function opcode_FD_x(second, third, fourth) {
+    const { text, size } = opcode_DD_FD_x(second, third, fourth);
+    /* Replace 'r' with IY */
+    return { text: text.replace('r', 'IY'), size };
 }
