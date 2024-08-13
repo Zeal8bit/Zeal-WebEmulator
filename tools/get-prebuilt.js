7804db30591b91ac34582e3cf9c383866453518f
David Higgins <zoul0813@me.com>
2024-08-13 09:11:55 -0400

    * add better error handling, redirect support

--- tools/get-prebuilt.js
+++ tools/get-prebuilt.js
@@ -5,19 +5,35 @@ console.log("Getting Pre-built roms");
 
 const prebuilt_json_url = "https://zeal8bit.com/roms/index.json";
 
-const getRom = async (rom) => {
+const getRom = async (rom, remote = null) => {
   const fileName = rom.urls.split("/").slice(-1)[0];
-  const romPath = `roms/${fileName}`
+  // rename files to match "unique" name, to avoid conflicts
+  const romPath = `roms/${rom.name}.img`
 
-  if (fs.existsSync(`roms/${fileName}`)) {
-    console.log(`Exists roms/${fileName}`);
+  if (fs.existsSync(romPath)) {
+    console.log(`Exists ${romPath}`);
     rom.urls = romPath;
     return rom;
   }
 
+  const url = remote ? remote : rom.urls;
+
   return new Promise((resolve, reject) => {
-    https.get(rom.urls, (res) => {
-      process.stdout.write(`Downloading ${rom.urls} `);
+    process.stdout.write(`Downloading ${url} `);
+    https.get(url, (res) => {
+      // handle redirects
+      if([301,302,307,308].includes(res.statusCode)) {
+        process.stdout.write('->\n');
+        resolve(getRom(rom, res.headers.location));
+        return;
+      }
+
+      if(res.statusCode !== 200) {
+        process.stdout.write(`... Failed ${res.statusCode}\n`);
+        reject('file not found');
+        return;
+      }
+
       const content = [];
       res
         .on("data", (chunk) => {
@@ -32,12 +48,19 @@ const getRom = async (rom) => {
         }).on('error', (err) => {
           reject(err);
         });
+    }).on('error', (e) => {
+      console.log(`An error occurred downloading ${url}`);
+      reject(e);
     });
   });
 };
 
 (async () => {
-  const data = await fetch(prebuilt_json_url);
+  const data = await fetch(prebuilt_json_url).catch((err) => {
+    console.log(`An error occurred downloading ${prebuilt_json_url}`);
+    console.error(err);
+    process.exit(1);
+  });
   json = await data.json();
 
   const indexFile = {
@@ -47,25 +70,36 @@ const getRom = async (rom) => {
     stable: [],
   };
 
+  console.log('\nLatest:');
   if (json.latest) {
     indexFile.latest = await getRom(json.latest).catch((err) => {
-      return json.latest;
+      return null;
     });
   }
+  console.log('\nNightly:');
   if (json.nightly?.length > 0) {
     for (let i = 0; i < json.nightly.length; i++) {
-      indexFile.nightly.push(await getRom(json.nightly[i]).catch((err) => {
-        return json.nightly[i];
-      }));
+      let rom = await getRom(json.nightly[i]).catch((err) => {
+        return null;
+      })
+      if(rom) {
+        indexFile.nightly.push(rom);
+      }
     }
   }
+  console.log('\nStable:');
   if (json.stable?.length > 0) {
     for (let i = 0; i < json.stable.length; i++) {
-      indexFile.stable.push(await getRom(json.stable[i]).catch((err) => {
-        return json.stable[i];
-      }));
+      let rom = await getRom(json.stable[i]).catch((err) => {
+        return null;
+      });
+      if(rom) {
+        indexFile.stable.push(rom);
+      }
     }
   }
 
-  fs.writeFileSync(`roms/index.json`, JSON.stringify(indexFile, false, 2));
+  console.log('\n\nWriting roms/index.json');
+  fileString = JSON.stringify(indexFile, false, 2);
+  fs.writeFileSync(`roms/index.json`, fileString);
 })();
