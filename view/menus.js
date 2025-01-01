3de058872018f0cae93d76fa3bd19b04c0c237ad
David Higgins <zoul0813@me.com>
2025-01-01 12:11:34 -0500

    * "Paste to Console" to allow pasting text into Zeal from the Host

--- view/menus.js
+++ view/menus.js
@@ -153,6 +153,135 @@ $('#hostfs-mount').on('click', () => {
     }
 });
 
+const KC_SHIFT = 16;
+const charsToKeycode = {
+    " ": [32],
+    "!": [KC_SHIFT, 49],
+    "@": [KC_SHIFT, 50],
+    "#": [KC_SHIFT, 50],
+    "$": [KC_SHIFT, 52],
+    "%": [KC_SHIFT, 53],
+    "^": [KC_SHIFT, 54],
+    "&": [KC_SHIFT, 55],
+    "*": [KC_SHIFT, 56],
+    "(": [KC_SHIFT, 57],
+    ")": [KC_SHIFT, 48],
+    "'": [222],
+    "\"": [KC_SHIFT, 222],
+    "=": [187],
+    "+": [KC_SHIFT, 187],
+    "-": [189],
+    "/": [191],
+    "0": [48],
+    "1": [49],
+    "2": [50],
+    "3": [51],
+    "4": [52],
+    "5": [53],
+    "6": [54],
+    "7": [55],
+    "8": [56],
+    "9": [57],
+    ";": [186],
+    ":": [KC_SHIFT, 186],
+    ",": [188],
+    "<": [KC_SHIFT, 188],
+    ".": [190],
+    ">": [KC_SHIFT, 190],
+    "?": [KC_SHIFT, 191],
+    "A": [KC_SHIFT, 65],
+    "B": [KC_SHIFT, 66],
+    "C": [KC_SHIFT, 67],
+    "D": [KC_SHIFT, 68],
+    "E": [KC_SHIFT, 69],
+    "F": [KC_SHIFT, 70],
+    "G": [KC_SHIFT, 71],
+    "H": [KC_SHIFT, 72],
+    "I": [KC_SHIFT, 73],
+    "J": [KC_SHIFT, 74],
+    "K": [KC_SHIFT, 75],
+    "L": [KC_SHIFT, 76],
+    "M": [KC_SHIFT, 77],
+    "N": [KC_SHIFT, 78],
+    "O": [KC_SHIFT, 79],
+    "P": [KC_SHIFT, 80],
+    "Q": [KC_SHIFT, 81],
+    "R": [KC_SHIFT, 82],
+    "S": [KC_SHIFT, 83],
+    "T": [KC_SHIFT, 84],
+    "U": [KC_SHIFT, 85],
+    "V": [KC_SHIFT, 86],
+    "W": [KC_SHIFT, 87],
+    "X": [KC_SHIFT, 88],
+    "Y": [KC_SHIFT, 89],
+    "Z": [KC_SHIFT, 90],
+    "[": [219],
+    "\\": [220],
+    "]": [221],
+    "_": [KC_SHIFT, 189],
+    "`": [192],
+    "a": [65],
+    "b": [66],
+    "c": [67],
+    "d": [68],
+    "e": [69],
+    "f": [70],
+    "g": [71],
+    "h": [72],
+    "i": [73],
+    "j": [74],
+    "k": [75],
+    "l": [76],
+    "m": [77],
+    "n": [78],
+    "o": [79],
+    "p": [80],
+    "q": [81],
+    "r": [82],
+    "s": [83],
+    "t": [84],
+    "u": [85],
+    "v": [86],
+    "w": [87],
+    "x": [88],
+    "y": [89],
+    "z": [90],
+    "{": [KC_SHIFT, 219],
+    "|": [KC_SHIFT, 220],
+    "}": [KC_SHIFT, 221],
+    "~": [KC_SHIFT, 192],
+    "\t": [9],
+    "\n": [13], // Enter
+  };
+
+$("#console-paste").on("click", async () => {
+    $('#console-paste').blur();
+    let text = null;
+    try {
+        text = await navigator.clipboard.readText();
+    } catch (err) {
+        alert("Failed to read clipboard");
+    }
+    if (!text) return;
+
+    console.log("paste", text);
+    for (ch of text) {
+        const keyCodes = charsToKeycode[ch] ?? [];
+        for(keyCode of keyCodes) {
+            // const e = jQuery.Event("keydown", { keyCode });
+            // $("#screen").trigger(e);
+            const handled = zealcom.KeyboardKeyPressed(keyCode);
+        }
+        for(let i = keyCodes.length; i > 0; i--) {
+            keyCode = keyCodes[i-1];
+            // const e = jQuery.Event("keyup", { keyCode });
+            // $("#screen").trigger(e);
+            const handled = zealcom.KeyboardKeyReleased(keyCode);
+        }
+    }
+    $('#screen').focus();
+});
+
 $('#container .close').on('click', () => {
     $('#toppanel').toggleClass('theater-mode');
 })
